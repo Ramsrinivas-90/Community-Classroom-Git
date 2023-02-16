1be5fab557a2b9ea9aaa1aa1ccef47d54679a6fb
Aravinth Kumar <87023106+aravinthkumarms@users.noreply.github.com>
2023-02-16 19:14:25 +0530

    added global port value

--- env.py
+++ env.py
@@ -2,7 +2,10 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 PYRODBUserName = config['PYROUSERNAME']
 PYRODBPassword = config['PYROPASSWORD']
+PYRODBPORT = config['PYROPORT']
 BVDBUserName = config['BVUSERNAME']
 BVDBPassword = config['BVPASSWORD']
+BVDBPORT = config['BVPORT']
 BBDBUserName = config['BBUSERNAME']
 BBDBPassword = config['BBPASSWORD']
+BBDBPORT = config['BBPORT']
