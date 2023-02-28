9ed0606770c91bb9d23870983add2014450090e7
Aravinth Kumar <aravinth.kumar@mrcooper.com>
2023-02-28 16:18:13 +0530

    Added file path and mail to env and removed hardcorded value from main

--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
 import win32com.client as client
+
+from utils.Env import MailTo, FilePath
 from utils.HTMLGenerator import *
 
 outlook = client.Dispatch('Outlook.Application')
 mail = outlook.CreateItem(0)
 mail.display()
-mail.To = 'Aravinth Kumar'
+mail.To = MailTo
 mail.Subject = 'Pyro BV Flow'
 
 mail.htmlbody = HTMLBody
-mail.Attachments.Add(
-    'C:/Users/akumar45/OneDrive - Mr. Cooper/PYROBVMailScript/body.txt')
+mail.Attachments.Add(FilePath)
 if mail.send:
     print('Mail Sent')
