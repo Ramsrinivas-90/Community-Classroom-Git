5ede07a683dccee776e446eda955958b1e7ea033
Aravinth Kumar <aravinth.kumar@mrcooper.com>
2023-02-28 17:25:37 +0530

    env value change

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import win32com.client as client
+
+from utils.Env import MailTo
 from utils.HTMLGenerator import htmlbody
 
 outlook = client.Dispatch('Outlook.Application')
 mail = outlook.CreateItem(0)
 mail.display()
-mail.To = 'Aravinth Kumar'
+mail.To = MailTo
 mail.Subject = 'Status On Production Blobs'
 mail.htmlbody = htmlbody
 
